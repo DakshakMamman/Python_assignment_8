60392b894ab1be38b1d57ea800e220e48ed7ffb3
Lere Dakshak Mamman <dakshaklere02@gmail.com>
2025-09-04 21:10:13 +0100

    Feat: banking system function implement

---
+++ bank.py
@@ -0,0 +1,47 @@
+'''
+TASK: Create a BankAccount class.
+
+Requirements:
+1. Each account should have an owner name and a starting balance.
+2. Provide a method to deposit money.
+3. Provide a method to withdraw money (only if sufficient balance).
+4. Provide a method to check current balance.
+
+Example Usage:
+    acc = BankAccount("Alice", 1000)
+    acc.deposit(500)
+    acc.withdraw(200)
+    print(acc.get_balance())  # 1300
+'''
+
+class BankAccount:
+    print("Banking System")
+    print("==============")
+    print()
+    def __init__(self, owner, balance=0.0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited {amount}. New balance: {self.balance}")
+        else:
+            print("Deposit amount must be positive")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.balance}")
+        else:
+            print("Insufficient funds")
+
+    def get_balance(self):
+        return self.balance
+
+
+
+acc = BankAccount("Alice", 1000)
+acc.deposit(500)       
+acc.withdraw(200)      
+print(acc.get_balance())  
